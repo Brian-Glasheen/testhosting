b1f79da30bd2bc1b49e011e2648fb0b0982fa3a8
nmlud21 <nickl239@att.netz>
2023-10-30 14:17:51 -0500

    Updated home page to not contain links

--- react-setup/ui/src/Home.js
+++ react-setup/ui/src/Home.js
@@ -1,11 +1,8 @@
-import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <h1>This is the home page</h1>
-      <Link to="menu">Click to view our menu page</Link>
-      <Link to="selfserve">Click to view our selfserve page</Link>
     </div>
   );
 }
