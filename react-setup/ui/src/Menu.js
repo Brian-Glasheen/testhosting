7592a39617f41e25ff39be1dd5223b472e676d93
rmalani26 <rmalani@tamu.edu>
2023-10-30 13:34:06 -0500

    added weather api

--- react-setup/ui/src/Menu.js
+++ react-setup/ui/src/Menu.js
@@ -1,17 +1,24 @@
 // import React from 'react';
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Menu() {
     const [jsonData, setJsonData] = useState(null);
+    const [weatherData, setWeatherData] = useState(null);
+    const weatherApiKey = '1927c85cb52bc4610c023263a7b8a293';
     
     useEffect(() => {
         fetch('http://127.0.0.1:8000/api/menus/?format=json') // Replace with your API URL
         .then((response) => response.json())
         .then((data) => setJsonData(data))
         .catch((error) => console.error('Error fetching data:', error));
+
+        axios.get(`http://api.weatherstack.com/current?access_key=${weatherApiKey}&query=College Station`)
+      .then((data) => setWeatherData(data))
+      .catch((error) => console.error('Error fetching weather data:', error));
     }, []);
     
-    if (!jsonData) {
+    if (!jsonData || ! weatherData) {
         return <div>Loading data...</div>;
         // return ;
     }
@@ -29,6 +36,12 @@ function Menu() {
   return (
     <div>
       <h1>Menu</h1>
+        <div>
+          <h2>Weather Information</h2>
+          <p>City: {weatherData.location.name}</p>
+          <p>Temperature: {weatherData.current.temperature} &deg;C</p>
+          <p>Weather: {weatherData.current.weather_descriptions[0]}</p>
+        </div>
       {Object.entries(groupedData).map(([category, items]) => (
         <div key={category}>
           <h2>{category}</h2>
